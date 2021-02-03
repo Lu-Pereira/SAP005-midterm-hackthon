4b443020ce35e63d46fcda3e57b45b1e65a95d2d
KarinaFS <karina_fsantos@outlook.com>
2021-02-02 21:46:17 -0300

    função printar na tela e busca

---
+++ src/data/livetube.js
@@ -0,0 +1,35 @@
+export default {
+    "results": [
+        {
+            "artist": "Ludmilla",
+            "title": "Live da Ludmilla com no Festival Black November",
+            "image": "https://raw.githubusercontent.com/Lu-Pereira/SAP005-midterm-hackthon/main/src/img/img-mock/ludmillaliveA.jpeg",
+            "date": "20 - 11 - 2020",
+            "information": "Ludmilla da um show em mais uma live especial no Festival Black November",
+            "url": "https://www.youtube.com/watch?v=oK2c3SGNExY",
+
+        },
+
+        {
+            "artist": "Ludmilla",
+            "title": "Live ao vivo Ludmila cantando em casa",
+            "image": "https://raw.githubusercontent.com/Lu-Pereira/SAP005-midterm-hackthon/main/src/img/img-mock/ludmillaliveB.jpeg",
+            "date": "10 - 01 - 2021",
+            "information": "Ludmilla cantando seus maiores sucessos #LIVEDALUD",
+            "url": "https://www.youtube.com/watch?v=j5w5oH5qNPA"
+
+        },
+
+
+        {
+            "artist": "Ludmilla",
+            "title": "Livedalud",
+            "image": "https://raw.githubusercontent.com/Lu-Pereira/SAP005-midterm-hackthon/main/src/img/img-mock/ludmillaliveC.jpeg",
+            "date": "03 - 09 - 2020",
+            "information": "Arrasando até de baixo d'agua",
+            "url": "https://www.youtube.com/watch?v=tf8sKIw52uA&t=13s"
+
+        }
+
+    ]
+};
